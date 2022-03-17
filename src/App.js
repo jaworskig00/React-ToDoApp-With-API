8700182697c0fb5e405f750215211a22092d7377
jaworskig00 <2000grzegorz@gmail.com>
2022-03-17 14:59:25 +0100

    Modified App.js, added utils/api.js and deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
+import { useEffect, useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+import { api } from './utils/api';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const fetchTodos = async () => {
+    try {
+      const { data } = await api.get("/task");
+      if (data.data) {
+        setTodos(data.data);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const initialValues = {
+    description: ""
+  };
+
+  const handleSubmit = (values, { setSubmitting, resetForm }) => {
+    api.post("/task", {"description": values.description});
+    fetchTodos();
+    resetForm({});
+    console.log(values.description);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>TODO LIST</header>
+      <main>
+        <ul>
+          {todos.length ? (
+            todos.map((todo) => {
+              return <li key={todo.id}>{todo.description}</li>; //Id: {todo._id}
+            })
+          ) : (
+            <li>No todos yet</li>
+          )}
+        </ul>
+      </main>
+      <aside style={{ paddingLeft: "10px" }}>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+          <Form>
+            <label htmlFor="description">Add description of task</label>
+            <br/>
+            <Field name="description" type="text" placeholder="description" />
+            <button type="submit">Add</button>
+          </Form>
+        </Formik>
+      </aside>
     </div>
   );
 }
---
+++ src/utils/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjMzMmE0NzcxNjFhZDAwMTc3ZDJiM2IiLCJpYXQiOjE2NDc1MjAzMjd9.vJC6TgD-_DHjUeGGn1PN1qgRj6wR4QUU5evP1h9Drb0"
+
+export const api = axios.create({
+    baseURL: "https://api-nodejs-todolist.herokuapp.com/",
+    headers: {"Authorization": "Bearer " + token}
+})
