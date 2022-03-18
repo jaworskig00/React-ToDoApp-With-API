5f7dc74b81c17011944548c4801a56aabfc3889a
jaworskig00 <2000grzegorz@gmail.com>
2022-03-18 14:45:32 +0100

    Added routes to index.js, added components for registration, login and blankpage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./index.css";
+import App from "./App";
+import LoginFlow from "./routes/LoginFlow";
+import { BlankPage } from "./routes/BlankPage";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginFlow />} />
+        <Route path="/todos" element={<App />} />
+        <Route path="*" element={<BlankPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
