8e015b07292b35ad27ca1f79870c030e24833c90
jaworskig00 <2000grzegorz@gmail.com>
2022-03-21 19:44:44 +0100

    Added register validation with yup

--- src/components/LoginFlow/Register/Register.js
+++ src/components/LoginFlow/Register/Register.js
@@ -1,65 +1,118 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { Link } from "react-router-dom";
+import * as Yup from "yup";
+import { Link, useNavigate } from "react-router-dom";
+
+import { api } from "../../../utils/api";
 
 function Register({ handleSwitch }) {
+  const navigate = useNavigate();
+
   const initialValues = {
     name: "",
     email: "",
     password: "",
-    age: undefined,
+    age: "",
   };
+
+  const RegisterSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, "Name too short - must have at least 3 characteres")
+      .max(30, "Name too long - must have less than 31 characteres")
+      .required("Name is required"),
+    email: Yup.string().email("Invalid email").required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password too short - must have at least 6 characteres")
+      .required("Password is required"),
+    age: Yup.number()
+      .positive("Age must be a positive number")
+      .integer("Age must be an integer")
+      .required("Age is required"),
+  });
 
   return (
     <div className="p-6 max-w-lg mx-auto bg-white rounded-xl shadow-lg items-center space-y-4">
       <aside className="border rounded-lg p-3">
-        <Formik initialValues={initialValues} /*onSubmit={handleSubmit}*/>
-          <Form className="min-w-max mx-auto flex items-center flex-col">
-            <label className="text-2xl underline mx-auto" htmlFor="description">
-              Please register
-            </label>
-            <br />
-            <Field
-              className=" py-1 px-3 m-2 placeholder:italic bg-white border rounded-full w-11/12"
-              name="name"
-              type="text"
-              placeholder="Name"
-            />
-            <Field
-              className=" py-1 px-3 m-2 placeholder:italic bg-white border rounded-full w-11/12"
-              name="email"
-              type="email"
-              placeholder="email@example.com"
-            />
-            <Field
-              className=" py-1 px-3 m-2 placeholder:italic bg-white border rounded-full w-11/12"
-              name="password"
-              type="password"
-              placeholder="Password"
-            />
-            <Field
-              className=" py-1 px-3 m-2 placeholder:italic bg-white border rounded-full w-11/12"
-              name="age"
-              type="number"
-              placeholder="Age"
-            />
-            <br />
-            <button
-              className="px-3 py-1 m-2 text-sm text-purple-600 font-semibold border rounded-full border-purple-200 hover:text-white hover:bg-purple-600"
-              type="submit"
-            >
-              <Link to={"/todos"}>Register</Link>
-            </button>
-            <button
-              className="underline"
-              name="switchButton"
-              type="text"
-              onClick={() => {
-                handleSwitch();
-              }}
-            >
-              Login
-            </button>
-          </Form>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={RegisterSchema}
+          /*onSubmit={handleSubmit}*/
+        >
+          {({ errors, touched, isValid, dirty }) => (
+            <Form className="min-w-max mx-auto flex items-center flex-col">
+              <label
+                className="text-2xl underline mx-auto"
+                htmlFor="description"
+              >
+                Please register
+              </label>
+              <br />
+              <Field
+                className=" py-1 px-3 mx-2 mt-2 placeholder:italic bg-white border rounded-full w-11/12"
+                name="name"
+                type="text"
+                placeholder="Name"
+              />
+              {errors.name && touched.name ? (
+                <div className="py-1 px-3 mx-2 mb-2 w-11/12 text-red-600">
+                  {errors.name}
+                </div>
+              ) : null}
+              <Field
+                className=" py-1 px-3 mx-2 mt-2 placeholder:italic bg-white border rounded-full w-11/12"
+                name="email"
+                type="email"
+                placeholder="email@example.com"
+              />
+              {errors.email && touched.email ? (
+                <div className="py-1 px-3 mx-2 mb-2 w-11/12 text-red-600">
+                  {errors.email}
+                </div>
+              ) : null}
+              <Field
+                className=" py-1 px-3 mx-2 mt-2 placeholder:italic bg-white border rounded-full w-11/12"
+                name="password"
+                type="password"
+                placeholder="Password"
+              />
+              {errors.password && touched.password ? (
+                <div className="py-1 px-3 mx-2 mb-2 w-11/12 text-red-600">
+                  {errors.password}
+                </div>
+              ) : null}
+              <Field
+                className=" py-1 px-3 mx-2 mt-2 placeholder:italic bg-white border rounded-full w-11/12"
+                name="age"
+                type="number"
+                placeholder="Age"
+              />
+              {errors.age && touched.age ? (
+                <div className="py-1 px-3 mx-2 mb-2 w-11/12 text-red-600">
+                  {errors.age}
+                </div>
+              ) : null}
+              <br />
+              <button
+                className="px-3 py-1 m-2 text-sm text-purple-600 font-semibold border rounded-full border-purple-200 hover:text-white hover:bg-purple-600"
+                type="submit"
+                disabled={!(dirty && isValid)}
+                onClick={() => {
+                  navigate("/todos");
+                }}
+              >
+                Register
+              </button>
+              <button
+                className="underline"
+                name="switchButton"
+                type="button"
+                onClick={() => {
+                  handleSwitch();
+                }}
+              >
+                Login
+              </button>
+            </Form>
+          )}
         </Formik>
       </aside>
     </div>
